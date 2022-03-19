8a3e7062c40761c9550a7830d50ee9298cfd0e1d
Mukesh S <92714633+Mukesh-Sonu@users.noreply.github.com>
2022-03-19 23:38:25 +0530

    completed the basic logics

--- src/App.js
+++ src/App.js
@@ -1,56 +1,96 @@
 import "./App.css";
-
 import { useState } from "react";
-function App() {
-  const [displayValue, setDisplayValue] = useState("");
 
+function App() {
+  const [firstNum, setFirstNum] = useState("");
+  const [secondNum, setSecondNum] = useState("");
+  const [operator, setOperator] = useState("");
+  const [total, setTotal] = useState("");
+  const [result, setResult] = useState("");
+
+  const buttonArr = [
+    "1",
+    "2",
+    "3",
+    "+",
+    "4",
+    "5",
+    "6",
+    "-",
+    "7",
+    "8",
+    "9",
+    "*",
+    "Clear",
+    "0",
+    "=",
+    "/"
+  ];
   const operators = ["+", "-", "*", "/"];
 
+  const basicfun = (firNum, secNum, opr) => {
+    let fNum = parseInt(firNum);
+    let sNum = parseInt(secNum);
+
+    switch (opr) {
+      case "+":
+        return fNum + sNum;
+      case "-":
+        return fNum - sNum;
+      case "*":
+        return fNum * sNum;
+      case "/":
+        return fNum / sNum;
+      default:
+        return null;
+    }
+  };
   const clickedValues = (value) => {
-    // this condition removes the duplicate operators and avoids displaying operator for the first time
     if (
-      (operators.includes(value) && displayValue === "") ||
-      (operators.includes(value) && operators.includes(displayValue.slice(-1)))
+      (operators.includes(value) && result === "") ||
+      (operators.includes(value) && operators.includes(result.slice(-1)))
     )
       return;
-
-    setDisplayValue(displayValue + value);
-  };
-
-  //looping Numbers...
-  const numbers = () => {
-    const numArr = [];
-    for (let i = 1; i < 10; i++) {
-      numArr.push(
-        <button key={i} onClick={() => clickedValues(i.toString())}>
-          {i}
-        </button>
-      );
+    if (firstNum === "" || (secondNum !== "" && !operators.includes(value))) {
+      console.log(value, "baadu");
+      setResult(value);
+      setFirstNum(value);
     }
-    return numArr;
+    if (secondNum === "") {
+      setSecondNum(value);
+    }
+    if (operators.includes(value)) {
+      if (firstNum && secondNum && operator) {
+        console.log(firstNum, secondNum, operator);
+        let output = basicfun(firstNum, secondNum, operator);
+        setResult(output.toString());
+        setTotal(output);
+      }
+      setOperator(value);
+    }
+    if (!operators.includes(value)) {
+      console.log(value, "val");
+      setResult(value);
+    }
+
+    if (value === "=") {
+      setResult(total);
+    }
   };
 
   return (
-    <div className="container">
-      <div className="calculator">
-        <div className="display">{displayValue}</div>
-
-        <div className="split">
-          <div className="numbers">
-            {numbers()}
-            <button onClick={() => clickedValues("clr")}>Clear</button>
-            <button onClick={() => clickedValues("0")}>0</button>
-            <button onClick={() => clickedValues("=")}>=</button>
-          </div>
-          <div className="operators">
-            <button onClick={() => clickedValues("+")}>Add (+)</button>
-            <button onClick={() => clickedValues("-")}>Sub (-)</button>
-            <button onClick={() => clickedValues("*")}>Mul (*)</button>
-            <button onClick={() => clickedValues("/")}>Div (/)</button>
-          </div>
+    <>
+      <div className="container">
+        <div className="display">{result || "0"}</div>
+        <div className="buttons">
+          {buttonArr.map((val, index) => (
+            <button key={index} onClick={() => clickedValues(val)}>
+              {val}
+            </button>
+          ))}
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
